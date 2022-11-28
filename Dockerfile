b2cdca4beb69f507d9ffdf7c16a0143aafe56c17
shubham <shubhamwalunj25@gmail.com>
2022-11-28 16:29:58 +0530

    added dockerfile to commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:11
+EXPOSE 8080
+ADD target/docker-project.jar docker-project.jar
+ENTRYPOINT ["java","-jar","/docker-project.jar"]
--- src/main/java/com/testdocker/docker_project/Test.java
+++ src/main/java/com/testdocker/docker_project/Test.java
@@ -1,5 +1,8 @@
 package com.testdocker.docker_project;
 
 public class Test {
-
+	int x=10;
+	public Test() {
+		System.out.println("We got it ="+x);
+	}
 }
